6074a024cc184e9146e8e29db49578c79667343b
noam <noamzvi22@gmail.com>
2023-08-07 07:53:16 +0300

    Feat(qr): add score and credit from scan

--- Front/src/components/QR/QrScanner.tsx
+++ Front/src/components/QR/QrScanner.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import "./QrScanner.css";
+import { UserContext } from "../../contexts/userContext";
 
 export default function QrScanner() {
   const [scanResult, setScanResult] = useState<string | null>(null);
+  const { user, setUser } = useContext(UserContext);
 
   useEffect(() => {
     const scanner = new Html5QrcodeScanner(
@@ -20,6 +22,7 @@ export default function QrScanner() {
     function success(result: string) {
       scanner.clear();
       setScanResult(result);
+      setUser({ ...user, score: user.score + 30, credit: user.credit + 20 });
     }
 
     function error(err: string) {
@@ -34,7 +37,14 @@ export default function QrScanner() {
   return (
     <div className="qr-container">
       <div className="qr-box">
-        {scanResult ? <div>{scanResult}</div> : <div id="reader"></div>}
+        {scanResult ? (
+          <div className="qr-box__message">
+            <span>נוספו לך 30 נקודות xp</span>
+            <span> ו20 אקוינס!</span>
+          </div>
+        ) : (
+          <div id="reader"></div>
+        )}
       </div>
     </div>
   );
