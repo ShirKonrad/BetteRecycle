d8f1acc33de715617de70280e37980dc3bec0461
yahel <yahelicoco@gmail.com>
2023-08-07 06:04:25 +0300

    add transicition

--- Front/src/components/Product/Product.tsx
+++ Front/src/components/Product/Product.tsx
@@ -6,32 +6,37 @@ interface Props {
   price: number;
   amount?: number;
   expartionDate?: Date;
+  code: string;
   image: string;
 }
 
 const Product: React.FC<Props> = (props: Props) => {
   return (
-    <div className="product">
-      <img
-        src={require(`C:/Users/yahel/OneDrive/שולחן העבודה/BetteRecycle/Front/src/assets/Images/Tsadkani.jpg`)}
-        alt="ma"
-      />
-      <p className="header niceText">{props.name}</p>
+    <div className="flip-card">
+      <div className="product flip-card-inner">
+        <img
+          src={require(`C:/Users/yahel/OneDrive/שולחן העבודה/BetteRecycle/Front/src/assets/Images/Tsadkani.jpg`)}
+          alt="ma"
+        />
+        <p className="header niceText">{props.name}</p>
 
-      {props.amount ? (
-        <p className="amount niceText">כמות: {props.amount}</p>
-      ) : (
-        <></>
-      )}
-      {props.expartionDate ? (
-        <p className="amount niceText">
-          תקף עד: {props.expartionDate.toLocaleDateString()}
-        </p>
-      ) : (
-        <></>
-      )}
+        {props.amount ? (
+          <p className="amount niceText">כמות: {props.amount}</p>
+        ) : (
+          <></>
+        )}
+        {props.expartionDate ? (
+          <p className="amount niceText">
+            תקף עד: {props.expartionDate.toLocaleDateString()}
+          </p>
+        ) : (
+          <></>
+        )}
 
-      <button className="price niceText">מימוש ההטבה</button>
+        <button className="price niceText flip-card-front">מימוש ההטבה</button>
+
+        <div className="flip-card-back">הקוד שלך: {props.code}</div>
+      </div>
     </div>
   );
 };
