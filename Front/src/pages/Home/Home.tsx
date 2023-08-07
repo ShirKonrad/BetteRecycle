fcc1728824dd39cb8ed1a786b08998cd4d3517f3
yoav koko <yoavkoko25@gmail.com>
2023-08-07 07:00:34 +0300

    Made homescreen look better

--- Front/src/pages/Home/Home.tsx
+++ Front/src/pages/Home/Home.tsx
@@ -7,7 +7,7 @@ interface Props {}
 const Home = function () {
   return (
     <Box>
-      <Typography variant="h2" sx={{ textAlign: "center", mt: 3 }}>
+      <Typography variant="h2" sx={{ textAlign: "center", mt: 8 }}>
         שלום, אביב!
       </Typography>
       <Box>
@@ -18,23 +18,23 @@ const Home = function () {
             m: 2,
             mt: 4,
           }}>
-          <Paper elevation={8} sx={{ mx: 2 }}>
+          <Paper elevation={8} sx={{ mx: 2, p: 1 }}>
             <Typography variant="h3">4</Typography>
-            <Typography variant="h5">מספר הקופונים שברשותך</Typography>
+            <Typography variant="h6">מספר הקופונים שברשותך</Typography>
           </Paper>
-          <Paper elevation={8} sx={{ mx: 2 }}>
+          <Paper elevation={8} sx={{ mx: 2, p: 1 }}>
             <Typography variant="h3">פנטה</Typography>
-            <Typography variant="h5">המשקה הממוחזר ביותר שלך</Typography>
+            <Typography variant="h6">המשקה הממוחזר ביותר שלך</Typography>
           </Paper>
         </Box>
         <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
-          <Paper elevation={8} sx={{ mx: 2 }}>
+          <Paper elevation={8} sx={{ mx: 2, p: 1 }}>
             <Typography variant="h3">27</Typography>
-            <Typography variant="h5">הממוצע העולמי של מחזור לחודש</Typography>
+            <Typography variant="h6">הממוצע העולמי של מחזור לחודש</Typography>
           </Paper>
-          <Paper elevation={8} sx={{ mx: 2 }}>
+          <Paper elevation={8} sx={{ mx: 2, p: 1 }}>
             <Typography variant="h3">29</Typography>
-            <Typography variant="h5">מספר הבקבוקים שמחזרת החודש</Typography>
+            <Typography variant="h6">מספר הבקבוקים שמחזרת החודש</Typography>
           </Paper>
         </Box>
       </Box>
